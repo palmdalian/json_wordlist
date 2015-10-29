d4c890606eb946ee6f5b7ea7d503a470e321e047
Michael Hand <mhand@fitbit.com>
2015-10-29 16:28:05 -0700

    Add alliteration generation

--- generate_random_phrase.py
+++ generate_random_phrase.py
@@ -10,12 +10,23 @@ def do_random():
 
 def get_random(pos):	
 	rand, word_types =  do_random()
-	while pos not in word_types or " " in rand:
+	while (pos not in word_types) and (" " in rand):
 		rand, word_types = do_random()
 	return rand
 
+def get_alliteration(firstLetter, pos):
+	rand, word_types =  do_random()
+	while rand[0] != firstLetter[0]:
+		rand, word_types = do_random()
+		while (pos not in word_types) and (" " in rand):
+			rand, word_types = do_random()
+	return rand
 
+#Alliteration
+firstword = get_random("adverb")
+secondword = get_alliteration(firstword,"adjective")
+thirdword = get_alliteration(firstword,"noun")
+print "%s %s %s" % (firstword,secondword,thirdword)
 
-phrase = get_random("adverb") + " " + get_random("adjective") + " " + get_random("noun")
-
-print phrase
+#Straight up random phrase
+print "%s %s %s" % (get_random("adverb"),get_random("adjective"),get_random("noun"))
